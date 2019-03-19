6462bfc07dd43093d56373eb4355a79cbe90d42a
Padarthi <spadarth@rcinnet.rockwellcollins.com>
2019-03-19 15:48:36 +0530

    trying commit

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -5,10 +5,48 @@ public class CricketCoach implements Coach {
 	//	define a private field
 	private FortuneService fortuneService;
 	
+//	add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	
+	
 	//	Create a no-arg constructor
 	public CricketCoach() {
-		System.out.println();
+		System.out.println("CricketCoach: Inside no-arg constructor");
 	}
+	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: Inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: Inside setter method - setTeam");
+		this.team = team;
+	}
+
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: Inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+
+
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minutes";
